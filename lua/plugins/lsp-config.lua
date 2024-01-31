3c24cbc4a07f751b969d36c7079c5b03a60744a5
stan-alexandru <stan.alexandru26@gmail.com>
2024-01-31 11:29:06 +0200

    add dashboard and config lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,17 @@ return {
 		"williamboman/mason.nvim",
 		lazy = false,
 		config = function()
-			require("mason").setup()
+			require("mason").setup(
+				{
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗"
+						}
+					}
+				}
+			)
 		end,
 	},
 	{
@@ -12,21 +22,40 @@ return {
 		opts = {
 			auto_install = true,
 		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "astro", "cssls", "eslint", "html", "jsonls", "tsserver", "tailwindcss" },
+				automatic_installation = true,
+			})
+		end
+
 	},
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
+			local lspconfig = require("lspconfig")
 			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.tsserver.setup({
+			lspconfig.astro.setup({
+				capabilities = capabilities
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.eslint.setup({
 				capabilities = capabilities
 			})
 			lspconfig.html.setup({
 				capabilities = capabilities
 			})
-			lspconfig.lua_ls.setup({
+			lspconfig.jsonls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities
+			})
+			lspconfig.tailwindcss.setup({
 				capabilities = capabilities
 			})
 
